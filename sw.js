9d3763834a22d7931101945343a49ccc55c29866
stpd1 <5735962+stpd1@users.noreply.github.com>
2024-08-19 13:06:00 +0200

    update sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = `stpd-calc`;
+
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll([
+            '/',
+        ]);
+    })());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+            return cachedResponse;
+        } else {
+            try {
+                const fetchResponse = await fetch(event.request);
+                cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+            } catch (e) {console.log("Failed fetching PWA response")}
+        }
+    })());
+});
