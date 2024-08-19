6581f2ecb97a80ec88dc937ec67ff1fddedaecf2
stpd1 <5735962+stpd1@users.noreply.github.com>
2024-08-19 13:22:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,6 +5,8 @@ self.addEventListener('install', event => {
         const cache = await caches.open(CACHE_NAME);
         cache.addAll([
             '/',
+            '/index.html',
+            '/sw.js',
         ]);
     })());
 });
