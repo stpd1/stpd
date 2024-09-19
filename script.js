7b8072bfb7fdef11ee7eea321ed435a77a7b168a
stpd1 <5735962+stpd1@users.noreply.github.com>
2024-09-19 11:56:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -288,6 +288,9 @@ const STDENV = {
 		PRINT(stringify(STK[STK.length-1]), "<pre>", "</pre>"); STK.pop()},
 	".": (STK,ENV)=> {assertStkl(1,STK); 
 		let ex = STK.pop(), tmpstk = []; ex.evaluate = true;
+		PRINT(stringify(ex) + " -> " + stringify(evals(ex, tmpstk)), "<pre>", "</pre>")},
+	"..": (STK,ENV)=> {assertStkl(1,STK); 
+		let ex = STK.pop(), tmpstk = []; ex.evaluate = true;
 		PRINT(stringify(ex) + " -> " + stringify(evaluate(ex, tmpstk, ENV)), "<pre>", "</pre>")},
 	"list": (STK,ENV)=> {assertStkl(1,STK);
 		let s = "<ul>", a = STK[STK.length-1]; STK.pop();
