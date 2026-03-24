39bd58036d1e1bc3cb28c4ae279c36b2395d846f
stpd1 <5735962+stpd1@users.noreply.github.com>
2026-03-24 20:14:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -516,7 +516,7 @@ function evaluate(ST, STK, ENV) {
 		for (let ei in ST) {
 			let e = ST[ei]; Array.isArray(e)?evaluate(e, a, ENV): a.push(e)}
 		STK.push(a)}
-	return STK[0];}
+	return STK;}
 function evals(s, STK = []) {
 	let ST = parse(tokenize(s));
 	return evaluate(ST, STK, ENV)}
